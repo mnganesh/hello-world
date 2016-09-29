67d393f42797ac5cfc44909656bb2d2344ca53a0
mnganesh <itsme@mnganesh.com>
2016-09-29 17:17:19 +0530

    Stub Code for Run Workflow

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,14 @@
+import java.io.*;
 /**
  * Created by mnganesh on 12/09/16.
  */
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello World");
+    public static void main(String[] args) throws IOException{
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter A :");
+				int a = Integer.parseInt(br.readLine());
+        System.out.println("Enter B:");
+				int b = Integer.parseInt(br.readLine());
+				System.out.println("Sum of A & B is " + Integer.toString(a+b));
     }
 }
